fe20f4ba6a52f19db7d469a7ed453dcdd5c46c39
Baraa <habbalb963@gmail.com>
2018-06-08 01:41:11 +0300

    created the movies service.

---
+++ Movies-Club/src/app/movies/shared/movies.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Movie } from './movie.model';
+import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
+
+@Injectable()
+export class MoviesService {
+  moviesList: AngularFireList<any>;
+  selectedMovie: Movie = new Movie();
+
+  constructor(private firebase : AngularFireDatabase) { }
+
+  getDate() {
+    this.moviesList = this.firebase.list('movies');
+    return this.moviesList;
+  }
+
+  insertMovie( movie: Movie ){
+     this.moviesList.push({
+      name: movie.name,
+      rating: movie.rating,
+      description: movie.description
+     });
+  }
+
+  updateMovie( movie: Movie ){
+    this.moviesList.update(movie.$key, 
+      {
+      name: movie.name,
+      rating: movie.rating,
+      description: movie.description
+      }
+    );
+  }
+
+  deleteMovie( $key: string){
+    this.moviesList.remove($key);
+  }
+
+
+
+
+
+
+}
